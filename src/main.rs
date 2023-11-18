87371f8ab530d8328b3647b607141f5114884c43
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2023-11-18 15:42:44 -0500

    Make sure all pieces are drawn right.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
+mod tetromino;
+
 use macroquad::prelude::*;
+use tetromino::Tetromino;
 
 #[macroquad::main("tetrs")]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let pieces: [Tetromino; 7] = tetromino::all_shapes();
+
     loop {
         clear_background(LIGHTGRAY);
+
+        for piece in &pieces {
+            piece.draw();
+        }
 
         next_frame().await;
     }
