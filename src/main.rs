be0a8ed7e0f36bb045b6b5d58298541ca92acddb
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2023-11-18 23:27:15 -0500

    Added controls to check rotation accuracy.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,39 @@
+mod game;
+mod square;
 mod tetromino;
 
 use macroquad::prelude::*;
 use tetromino::Tetromino;
 
 #[macroquad::main("tetrs")]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let pieces: [Tetromino; 7] = tetromino::all_shapes();
+async fn main() {
+    let mut pieces = tetromino::all_shapes();
+
+    let mut piece = &mut pieces[0];
 
     loop {
-        clear_background(LIGHTGRAY);
+        clear_background(WHITE);
 
-        for piece in &pieces {
-            piece.draw();
+        // select piece
+        if is_key_pressed(KeyCode::Key1) { piece = &mut pieces[0] }
+        if is_key_pressed(KeyCode::Key2) { piece = &mut pieces[1] }
+        if is_key_pressed(KeyCode::Key3) { piece = &mut pieces[2] }
+        if is_key_pressed(KeyCode::Key4) { piece = &mut pieces[3] }
+        if is_key_pressed(KeyCode::Key5) { piece = &mut pieces[4] }
+        if is_key_pressed(KeyCode::Key6) { piece = &mut pieces[5] }
+        if is_key_pressed(KeyCode::Key7) { piece = &mut pieces[6] }
+
+        // rotate the piece
+        if is_key_pressed(KeyCode::Z) {
+            piece.rotate_right();
         }
+        if is_key_pressed(KeyCode::X) {
+            piece.rotate_left();
+        }
+
+        // draw piece
+        piece.draw();
+        piece.draw_center(PINK);
 
         next_frame().await;
     }
