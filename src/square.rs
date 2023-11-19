a60504fa94fddf8b0f2775d93d1b7c3eaf8aff00
Daniel Anderson <daniel.j.anderson.dev@gmail.com>
2023-11-18 23:27:51 -0500

    A small wrapper around Rect ensuring w = h

---
+++ src/square.rs
@@ -0,0 +1,21 @@
+use macroquad::prelude::*;
+
+pub struct Square(Rect);
+impl Square {
+    pub fn new(x: f32, y: f32, size: f32) -> Square {
+        Square(
+            Rect { x, y, w: size, h: size }
+        )
+    }
+}
+impl std::ops::Deref for Square { // allows me to dereference any Square to get to the underlying Rect 
+    type Target = Rect;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+impl std::ops::DerefMut for Square {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
